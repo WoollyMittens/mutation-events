a3bbe1db8affd3f465ce0fda54b5a8becc700bb4
Maurice van Creij <maurice.vancreij@webqem.com>
2024-05-07 13:49:56 +1000

    Add comments

--- mutation-events.js
+++ mutation-events.js
@@ -1,18 +1,27 @@
 /*! Mutation Events v0.0.1 - (c) 2024 Maurice van Creij - MIT License - https://github.com/WoollyMittens */
 
+// public handler class
 export class MutationEvents {
 	constructor(options) {
+		// define the default options
 		const defaults = {
 			attributes: true,
 			childList: true,
 			subtree: true
 		}
+		// override the default options
 		this.options = {...defaults, ...options};
+		// start the intersection observer
 		this.observer = new MutationObserver(this.handle.bind(this));
 	}
+
+	// adds an element to the observed list
 	watch(elem, options) {
+		// observe the given element
 		this.observer.observe(elem, options || this.options);
 	}
+
+	// triggers an event for every mutation
 	handle(mutations, observer) {
 		for (let mutation of mutations) {
 			let evt = new CustomEvent("change", {
@@ -28,4 +37,5 @@ export class MutationEvents {
 	}
 }
 
+// expose the main class (disable if importing)
 window.MutationEvents = MutationEvents;
